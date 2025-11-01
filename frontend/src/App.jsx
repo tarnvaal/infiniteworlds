c2ea964455e2f4833f2c2a22176eca4a763ca49a
Eric Carpenter <ericcarpenter.mobile@gmail.com>
2025-10-31 21:33:50 -0700

    ux upgrade

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,9 +39,6 @@ function App() {
 
   return (
     <div style={{ maxWidth: "600px", margin: "2rem auto", fontFamily: "sans-serif" }}>
-      <h1>Persistent Dungeon Master</h1>
-      <p style={{ fontSize: "0.9rem", color: "#666" }}>Explore any world you can imagine</p>
-
       {errorMsg && <div style={{ color: "red", marginBottom: "1rem" }}>Error: {errorMsg}</div>}
 
       {responseText && (
